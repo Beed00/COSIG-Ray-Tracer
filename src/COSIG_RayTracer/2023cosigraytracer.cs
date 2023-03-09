9c82ba60838658fcf2e0715e66dcf6a983f6937b
Bernardo Monteiro <bernardosecamonteiro@gmail.com>
2023-03-09 22:09:44 +0000

    Implemented Load Button and Exit Button Functionalities

--- src/COSIG_RayTracer/2023cosigraytracer.cs
+++ src/COSIG_RayTracer/2023cosigraytracer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,24 @@ namespace COSIG_RayTracer
 
         private void loadButton_Click(object sender, EventArgs e)
         {
-
+            int size = -1;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            DialogResult result = openFileDialog.ShowDialog(); // Shows the dialog.
+            if (result == DialogResult.OK) // Tests result.
+            {
+                string file = openFileDialog.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine("FileSize: " + size);
+            Console.WriteLine("Result: " + result);
+            Console.WriteLine("FilePath: " + openFileDialog.FileName);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -44,7 +62,7 @@ namespace COSIG_RayTracer
 
         private void exitButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void progressBar_Click(object sender, EventArgs e)
